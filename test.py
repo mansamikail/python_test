b3f7ddd1ceedfc8c9f45eff8338f05714133a9cd
mansamikail <fifthharamone05@gmail.com>
2022-05-31 11:23:53 +0600

    plus

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-x1,y1 = list(map(float,input().split()))
-x2,y2 = list(map(float,input().split()))
-
-formula = ((x2-x1)**2 + (y2-y1)**2)**0.5
-print(f'{formula:.4f}')
+a = int(input('enter the number'))
+b = int(input('enter the number'))
+c = a+b
+print(c)
