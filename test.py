d2e2664c99e5127cd54269f069fce6be2d78728a
mansamikail <fifthharamone05@gmail.com>
2022-10-15 13:25:10 +0600

    sakib

--- test.py
+++ test.py
@@ -33,7 +33,7 @@
 #             print(' ', end='')
 #     print()
 
-x, y = list(map(int, input().split()))
-price = [0, 4, 4.50, 5, 2, 1.50]
-pay = price[x]*y
-print(f'Total: R$ {pay:.2f}')
+# x, y = list(map(int, input().split()))
+# price = [0, 4, 4.50, 5, 2, 1.50]
+# pay = price[x]*y
+# print(f'Total: R$ {pay:.2f}')
